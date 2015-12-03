b7ba839dde8a5255ebbc8f26e44c45a66f3f7f14
sgtcoolguy <chris.a.williams@gmail.com>
2015-12-03 16:18:42 -0500

    Create combined build script

--- build.sh
+++ build.sh
@@ -1,13 +1,28 @@
+rm -rf dist
 mkdir dist
 
+echo "Building Android module..."
 cd android
 ant
 cp dist/*.zip ../dist
-
 cd ..
+
+echo "Unzipping Android zipfile..."
+cd dist
+unzip hyperloop-android-*.zip
+rm hyperloop-android-*.zip
+cd ..
+
+echo "Building iOS module..."
 cd iphone
+VERSION=`grep "^version:" manifest | cut -c 10-`
 ./build.sh
-cp dist/*.zip ../dist
+cp -R build/zip/modules/iphone ../dist/modules
+cp -R build/zip/plugins/hyperloop ../dist/plugins/
 cd ..
 
-# TODO Combine the zips!
+echo "Creating combined zip with iOS and Android"
+cd dist
+zip -q -r hyperloop-$VERSION.zip *
+rm -rf modules
+rm -rf plugins
