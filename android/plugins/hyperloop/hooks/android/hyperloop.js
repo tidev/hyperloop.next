11f72c67adfa7902e796e87cb898067ae6eaa5b3
Christopher Williams <chris.a.williams@gmail.com>
2016-05-09 09:01:46 -0400

    [TIMOB-23266] Support com.example.* package requires - Bump to 1.2.0 with new wildcard package name requires in Android

--- android/plugins/hyperloop/hooks/android/hyperloop.js
+++ android/plugins/hyperloop/hooks/android/hyperloop.js
@@ -43,7 +43,7 @@ exports.cliVersion = '>=3.2';
 		jars = [],
 		aars = {},
 		cleanup = [],
-		requireRegex = /require\s*\([\\"']+([\w_\/-\\.]+)[\\"']+\)/ig;
+		requireRegex = /require\s*\([\\"']+([\w_\/-\\.\\*]+)[\\"']+\)/ig;
 
 	/*
 	 Config.
@@ -436,28 +436,51 @@ exports.cliVersion = '>=3.2';
 					found = [];
 				logger.trace('Searching for hyperloop requires in: ' + file);
 				(contents.match(requireRegex) || []).forEach(function (m) {
-					var re = /require\s*\([\\"']+([\w_\/-\\.]+)[\\"']+\)/i.exec(m),
+					var re = /require\s*\([\\"']+([\w_\/-\\.\\*]+)[\\"']+\)/i.exec(m),
 						className = re[1],
-						lastIndex;
+						lastIndex,
+						validPackage = false,
+						type,
+						ref,
+						str,
+						packageRegexp = new RegExp('^' + className.replace('.', '\\.').replace('*', '[A-Z]+[a-zA-Z0-9]+') + '$');
 
 					// Is this a Java type we found in the JARs/APIs?
 					logger.trace('Checking require for: ' + className);
-					var type = metabaseJSON.classes[className];
-					if (!type) {
-						// fallback for using dot notation to refer to nested class
-						lastIndex = className.lastIndexOf('.');
-						className = className.slice(0, lastIndex) + '$' + className.slice(lastIndex + 1);
+
+					// Look for requires using wildcard package names and assume all types under that namespace!
+					if (className.indexOf('.*') == className.length - 2) {
+						// Check that it's a valid package name and search for all the classes directly under that package!
+						for (var mClass in metabaseJSON.classes) {
+							if (mClass.match(packageRegexp)) {
+								found.push('hyperloop/' + mClass);
+								validPackage = true;
+							}
+						}
+						if (validPackage) {
+							ref = 'hyperloop/' + className.slice(0, className.length - 2); // drop the .* ending
+							str = "require('" + ref + "')";
+							contents = replaceAll(contents, m, str);
+						}
+					} else {
+						// single type
 						type = metabaseJSON.classes[className];
 						if (!type) {
-							return;
+							// fallback for using dot notation to refer to nested class
+							lastIndex = className.lastIndexOf('.');
+							className = className.slice(0, lastIndex) + '$' + className.slice(lastIndex + 1);
+							type = metabaseJSON.classes[className];
+							if (!type) {
+								return;
+							}
 						}
+						// Looks like it's a Java type, so let's hack it and add it to our list!
+						// replace the require to point to our generated file path
+						ref = 'hyperloop/' + className;
+						str = "require('" + ref + "')";
+						contents = replaceAll(contents, m, str);
+						found.push(ref);
 					}
-					// Looks like it's a Java type, so let's hack it and add it to our list!
-					// replace the require to point to our generated file path
-					var ref = 'hyperloop/' + className;
-					var str = "require('" + ref + "')";
-					contents = replaceAll(contents, m, str);
-					found.push(ref);
 				});
 				return [found, contents];
 			}
