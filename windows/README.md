7cf7ff89cc03853b250532ec96d764b8dddc93ff
Kota Iguchi <developer@infosia.co.jp>
2016-11-02 13:59:40 +0900

    Generate module configs on demand

--- windows/README.md
+++ windows/README.md
@@ -2,6 +2,17 @@
 
 This is the Hyperloop library for Windows
 
+# How to Build
+
+Make sure `cmake.exe` and `nuget.exe` are on your `PATH`. You can download `nuget.exe` via [https://dist.nuget.org/index.html](https://dist.nuget.org/index.html).
+
+```
+> cmake --version # Make sure you can run cmake
+> nuget help      # Make sure you can run nuget
+> cd hyperloop.next/windows
+> appc ti build -p windows --build-only -l trace
+```
+
 ## License
 
 See the [LICENSE](../LICENSE.md) text for full details.
--- windows/plugins/hooks/hyperloop.js
+++ windows/plugins/hooks/hyperloop.js
@@ -1,7 +1,9 @@
 var spawn = require('child_process').spawn,
-    path = require('path'),
     async = require('async'),
-    fs = require('fs');
+    path = require('path'),
+    fs   = require('fs'),
+    ejs  = require('ejs'),
+    appc = require('node-appc');
 
 exports.cliVersion = ">=3.2";
 exports.init = function(logger, config, cli, nodeappc) {
@@ -9,14 +11,33 @@ exports.init = function(logger, config, cli, nodeappc) {
      * CLI Hook for Hyperloop build dependencies
      */
     cli.on('build.module.pre.compile', function (data, callback) {
-        var dest = path.join(data.projectDir, 'reflection', 'HyperloopInvocation'),
-            tasks = [];
+        var tasks = [
+            function(next) {
+                generateCMakeList(data, next);
+            },
+            function(next) {
+                runCmake(data, 'WindowsStore', 'Win32', '10.0', next);
+            },
+            function(next) {
+                runCmake(data, 'WindowsStore', 'ARM', '10.0', next);
+            },
+            function(next) {
+                runCmake(data, 'WindowsPhone', 'Win32', '8.1', next);
+            },
+            function(next) {
+                runCmake(data, 'WindowsPhone', 'ARM', '8.1', next);
+            },
+            function(next) {
+                runCmake(data, 'WindowsStore', 'Win32', '8.1', next);
+            },
+        ];
 
+        var csharp_dest = path.join(data.projectDir, 'reflection', 'HyperloopInvocation');
         ['phone', 'store', 'win10'].forEach(function(platform) {
             ['Debug', 'Release'].forEach(function(buildConfig) {
                 tasks.push(
                     function(next) {
-                        buildSolution(data, dest, platform, buildConfig, next);
+                        buildSolution(data, csharp_dest, platform, buildConfig, next);
                     }
                 );
             });
@@ -47,6 +68,70 @@ exports.init = function(logger, config, cli, nodeappc) {
         callback(null, data);
     });
 };
+
+function generateCMakeList(data, next) {
+
+    var template  = path.join(data.projectDir, 'CMakeLists.txt.ejs'),
+        cmakelist = path.join(data.projectDir, 'CMakeLists.txt'),
+        windowsSrcDir = path.join(data.titaniumSdkPath, 'windows'),
+        version = data.manifest.version;
+
+    data.logger.debug('Updating CMakeLists.txt...');
+
+    fs.readFile(template, 'utf8', function (err, data) {
+        if (err) throw err;
+        data = ejs.render(data, { 
+            version: appc.version.format(version, 4, 4, true),
+            windowsSrcDir: windowsSrcDir.replace(/\\/g, '/').replace(' ', '\\ ')
+        }, {});
+
+        fs.writeFile(cmakelist, data, function(err) {
+            next(err);
+        });
+    });
+
+}
+
+function runCmake(data, platform, arch, sdkVersion, next) {
+    var logger = data.logger,
+        generatorName = 'Visual Studio 14 2015' + (arch==='ARM' ? ' ARM' : ''),
+        cmakeProjectName = (sdkVersion === '10.0' ? 'Windows10' : platform) + '.' + arch,
+        cmakeWorkDir = path.resolve(__dirname,'..','..',cmakeProjectName);
+
+    logger.debug('Run CMake on ' + cmakeWorkDir);
+
+    if (!fs.existsSync(cmakeWorkDir)) {
+        fs.mkdirSync(cmakeWorkDir);
+    }
+
+    var p = spawn('cmake.exe',
+        [
+            '-G', generatorName,
+            '-DCMAKE_SYSTEM_NAME=' + platform,
+            '-DCMAKE_SYSTEM_VERSION=' + sdkVersion,
+            '-DCMAKE_BUILD_TYPE=Debug',
+            path.resolve(__dirname,'..','..')
+        ],
+        {
+            cwd: cmakeWorkDir
+        });
+    p.on('error', function(err) {
+        logger.error(cmake);
+        logger.error(err);
+    });
+    p.stdout.on('data', function (data) {
+        logger.info(data.toString().trim());
+    });
+    p.stderr.on('data', function (data) {
+        logger.warn(data.toString().trim());
+    });
+    p.on('close', function (code) {
+        if (code != 0) {
+            process.exit(1); // Exit with code from cmake?
+        }
+        next();
+    });
+}
 
 function buildSolution(data, dest, platform, buildConfig, callback) {
     var slnFile = path.join(dest, platform, 'HyperloopInvocation.sln');
